ecf05bcfda5730abeda0f3ac26af09480d98a0ee
linglx <linglx_sti@126.com>
2018-03-29 21:58:54 +0800

    update: add a new method by #define in prompt_simple.h

--- main.cpp
+++ main.cpp
@@ -3,11 +3,19 @@
 //
 
 #include "prompt.h"
+#include "prompt_simple.h"
 
-using namespace prompt;
+using namespace prompt_simple;
 
 int main()
 {
+  INFO << "info" << '\t' << 2 << endl;
+  ERROR << "info" << '\t' << 2 << endl;
+  DEBUG << "info" << '\t' << 2 << endl;
+  WARNING << "info" << '\t' << 2 << endl;
+  NORMAL << "info" << '\t' << 2 << endl;
+
+  /*
   INFO() << "hello, world" << "\t" << 1 << '\n';
   INFO(CYAN) << "hello, world" << "\t" << 2 << '\n';
   INFO(NORMAL) << "-------------------------------" << '\n';
@@ -15,5 +23,6 @@ int main()
   WARNING() << "this is warning" << '\t' << 2 << '\n';
   DEBUG() << "this is debug message" << '\t' << 3 << '\n';
   ERROR() << "this is error message" << '\t' << 4 << '\n';
+  */
   return 0;
 }
